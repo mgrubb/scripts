1a21a9f1de3c70bee48b69f27d9b528b8c9dbd4d
Michael Grubb <devel@dailyvoid.com>
2012-04-27 12:03:36 -0500

    make EDITOR calls return to the terminal when done

--- vimedit.sh
+++ vimedit.sh
@@ -1,4 +1,3 @@
 #!/bin/ksh
 # Wrapper for MacVim on the command line, for use in the EDITOR variables
-/usr/bin/gvim --nomru -f "$@"
-/usr/local/bin/appswitch -f -a iTerm
+/usr/bin/gvim --nomru -c 'au VimLeave * call system("open -a iTerm")' -f "$@"
