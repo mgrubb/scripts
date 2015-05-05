0bba891e846f51238d44ad627ea12900d05eecb5
Michael Grubb <michael@grubb.co>
2015-05-05 11:04:01 -0500

    changed to use /usr/local/bin/mvim instead of symlink to mvim

--- vimedit.sh
+++ vimedit.sh
@@ -1,3 +1,3 @@
 #!/bin/ksh
 # Wrapper for MacVim on the command line, for use in the EDITOR variables
-/usr/bin/gvim -c 'au VimLeave * call system("open -a iTerm")' -f "$@"
+/usr/local/bin/mvim -c 'au VimLeave * call system("open -a iTerm")' -f "$@"
